3c944ae34d8642353e84bfe2dae5edd2b93ffc5e
M.Reza Sepehr <154729449+MRezaSep@users.noreply.github.com>
2024-10-16 08:15:14 +0330

    Delete public directory

--- public/V1/SplashScreen/script.js
+++
@@ -1,8 +0,0 @@
-function navigateToNextPage() {
-  window.location.href = "./../Home_Screen/home_screen/index.html";
-}
-window.onload = function () {
-  setTimeout(() => {
-    navigateToNextPage();
-  }, 2000);
-};
